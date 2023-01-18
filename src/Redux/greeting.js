edfe9c82ceb09ac77fc3661d2a45fa303b312c83
kusi <lawcubegsm@gmail.com>
2023-01-18 09:24:07 +0000

    Update api link

--- src/Redux/greeting.js
+++ src/Redux/greeting.js
@@ -1,7 +1,8 @@
 import {createSlice, createAsyncThunk } from '@reduxjs/toolkit'; 
 import axios from 'axios';
 
-const url = 'http://127.0.0.1:3000/api/v1/greetings';
+// const url = 'http://127.0.0.1:3000/';
+const url = 'http://127.0.0.1:3000/api/v1/messages/';
 
 const initialState = {
  greet: 'N/A',
